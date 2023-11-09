7c9c930898572ec91044650125b8b22aaedc5dbb
michaelwaves <68243408+michaelwaves@users.noreply.github.com>
2023-11-09 14:50:13 +0000

    sign in again

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
+import GetStarted from '@/components/user/GetStarted'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <GetStarted />
     </main>
   )
 }
