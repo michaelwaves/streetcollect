a6a6de17db590a2db47f4dd9a74f416c08e0859d
dalu46 <lawrencefranklin100@gmail.com>
2023-11-09 15:27:02 +0100

    created the first page

--- frontend/src/components/GetStarted.tsx
+++ frontend/src/components/GetStarted.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import smartImg from '../../public/SmartCollectLogo.png'
 
 const GetStarted = () => {
   return (
     <div>
       <h1>Hello, I'm a Next.js component!</h1>
+      <img src={smartImg} alt='smart logo' />
     </div>
   );
 };
